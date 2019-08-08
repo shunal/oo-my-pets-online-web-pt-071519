86f5dc35ec77369dd18c6d8724ef2161ee89a635
Steven Hunal <stevehunal@gmail.com>
2019-08-08 22:18:12 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,29 @@
+require "pry"
 class Owner
-  # code goes here
+  
+  attr_reader :name, :species
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+  end 
+  
+  def say_species
+    "I am a #{species}."
+  end
+  
+  def self.all 
+    @@all
+  end 
+  
+  def self.count
+    Owner.count
+  end
+  
+  
+  
+  
 end
