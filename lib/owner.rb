4d0eeb30d04cb66c1d6b53a56789e8cfd27c06c3
Steven Hunal <stevehunal@gmail.com>
2019-08-09 00:30:44 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -40,12 +40,43 @@ class Owner
   end 
   
   def buy_cat(name)
-    Cat.all.select do |cat|
-      if cat.name == name
-        cat.owner = self
-      end
-    end
+    Cat.new(name, self)
   end
   
+  def buy_dog(name)
+    Dog.new(name, self)
+  end
+  
+  def walk_dogs
+    self.dogs.each do |dog|
+      dog.mood = "happy"
+    end
+    end
+  
+  def feed_cats
+    self.cats.each do |cat|
+      cat.mood = "happy"
+    end
+  end 
+  
+  def sell_pets
+    self.cats.each do |cat|
+      cat.mood = "nervous"
+      cat.owner = nil
+    end
+    self.dogs.each do |dog|
+      dog.mood = "nervous"
+      dog.owner = nil
+    end
+  end 
+  
+  def list_pets
+    cats = self.cats.count
+    dogs = self.dogs.count
+    "I have #{dogs} dog(s), and #{cats} cat(s)."
+  end
+  
+  
+  # expect(@owner.list_pets).to eq("I have #{@owner.dogs.count} dog(s), and #{@owner.cats.count} cat(s).")
   
 end
